581e5cbda3026c423ec5819aa1778c17480faf08
Stanislaw Wegner <s.wegner@onet.eu>
2023-05-15 18:48:03 +0200

    view templates

---
+++ ColoradoBeetle.UI/Extensions/IServceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace ColoradoBeetle.UI.Extensions;
+public static class IServiceCollectionExtensions {
+
+    public static void DefineViewLocation(this IServiceCollection service,
+        IConfiguration configuration) {
+
+        var templateKey = configuration.GetSection("TemplateKey").Value;
+        service.Configure<RazorViewEngineOptions>(x => {
+
+            x.ViewLocationFormats.Clear();
+
+            if (templateKey != "Basic") {
+
+                x.ViewLocationFormats.Add("/Views/" + templateKey + "/{1}/{0}" + RazorViewEngine.ViewExtension);
+                x.ViewLocationFormats.Add("/Views/" + templateKey + "/Shared/{0}"
+                    + RazorViewEngine.ViewExtension);
+            }
+
+            x.ViewLocationFormats.Add("/Views/Basic/{1}/{0}" + RazorViewEngine.ViewExtension);
+            x.ViewLocationFormats.Add("/Views/Basic/Shared/{0}" + RazorViewEngine.ViewExtension);
+
+        });
+    }
+}
--- ColoradoBeetle.UI/Program.cs
+++ ColoradoBeetle.UI/Program.cs
@@ -1,5 +1,7 @@
 using ColoradoBeetle.Application;
 using ColoradoBeetle.Infrastructure;
+using ColoradoBeetle.UI.Extensions;
+using Microsoft.AspNetCore.Mvc.Razor;
 using NLog.Web;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,8 @@ builder.Logging.AddNLogWeb();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
+
+builder.Services.DefineViewLocation(builder.Configuration);
 
 builder.Services.AddControllersWithViews();
 
