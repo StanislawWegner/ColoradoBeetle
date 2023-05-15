408a53e49452d2425a2b11bf09465af315f7efff
Stanislaw Wegner <s.wegner@onet.eu>
2023-05-15 12:24:39 +0200

    added projects

---
+++ ColoradoBeetle.Application/DependecyInjection.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ColoradoBeetle.Application; 
+public static class DependecyInjection {
+    public static IServiceCollection AddApplication(this IServiceCollection services) {
+        return services;
+    }
+
+}
---
+++ ColoradoBeetle.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ColoradoBeetle.Infrastructure;
+public static class DependencyInjection {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+        return services;
+    }
+
+    public static IApplicationBuilder UseInfrstructure(this IApplicationBuilder app) {
+        return app;
+    }
+}
