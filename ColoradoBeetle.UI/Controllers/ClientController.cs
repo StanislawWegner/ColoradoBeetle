a61b6905fb703f464b3369374d95cb4f0e34f2fb
Stanislaw Wegner <s.wegner@onet.eu>
2023-06-15 21:55:01 +0200

    app main view template

---
+++ ColoradoBeetle.UI/Controllers/ClientController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColoradoBeetle.UI.Controllers {
+    public class ClientController : BaseController {
+        public IActionResult Dashboard() {
+            return View();
+        }
+    }
+}
