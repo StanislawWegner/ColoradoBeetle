5948fbb78824fe1439b96a1333b8e55412de7481
Stanislaw Wegner <s.wegner@onet.eu>
2023-06-16 13:58:08 +0200

    shift mainCarousel to View Component

---
+++ ColoradoBeetle.UI/Components/MainCarouselViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColoradoBeetle.UI.Components; 
+public class MainCarouselViewComponent : ViewComponent{
+    public async Task<IViewComponentResult> InvokeAsync(string testComponents) {
+        //logic
+        return View();
+    }
+}
