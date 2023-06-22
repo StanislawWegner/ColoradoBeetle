c86c6f6e349eb06159d1940ddae0576de443e9af
Stanislaw Wegner <s.wegner@onet.eu>
2023-06-22 13:45:16 +0200

    added EditRole view and logic

--- ColoradoBeetle.Infrastructure/Services/RoleManagerService.cs
+++ ColoradoBeetle.Infrastructure/Services/RoleManagerService.cs
@@ -34,4 +34,29 @@ public class RoleManagerService : IRoleManagerService {
 
         return _roleManager.Roles.Select(x => new RoleDto { Id = x.Id, Name = x.Name }).ToList();
     }
+
+    public async Task UpdateAsync(RoleDto role) {
+
+        var roleDb = await _roleManager.FindByIdAsync(role.Id);
+
+        if (roleDb.Name != role.Name)
+            await ValidateRoleName(role.Name);
+
+        roleDb.Name = role.Name;
+
+        var result = await _roleManager.UpdateAsync(roleDb);
+
+        if (!result.Succeeded)
+            throw new Exception(string.Join(". ", result.Errors.Select(x => x.Description)));
+    }
+
+    public async Task<RoleDto> FindByIdAsync(string id) {
+
+        var role = await _roleManager.FindByIdAsync(id);
+
+        if (role == null)
+            throw new Exception($"Brak roli o podanym id: {id}.");
+
+        return new RoleDto { Id = role.Id, Name = role.Name };
+    }
 }
--- ColoradoBeetle.UI/Controllers/RoleController.cs
+++ ColoradoBeetle.UI/Controllers/RoleController.cs
@@ -1,4 +1,6 @@
 ﻿using ColoradoBeetle.Application.Roles.Commands.AddRole;
+using ColoradoBeetle.Application.Roles.Commands.EditRole;
+using ColoradoBeetle.Application.Roles.Queries.GetEditRole;
 using ColoradoBeetle.Application.Roles.Queries.GetRoles;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,24 @@ public class RoleController : BaseController {
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> AddRole(AddRoleCommand command) {
+
+        var result = await MediatorSendValidate(command);
+
+        if (!result.IsValid)
+            return View(command);
+
+        TempData["Success"] = "Role zostały zaktualizowane";
+
+        return RedirectToAction("Roles");
+    }
+    public async Task<IActionResult> EditRole(string id) {
+
+        return View(await Mediator.Send(new GetEditRoleQuery { Id = id }));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> EditRole(EditRoleCommand command) {
 
         var result = await MediatorSendValidate(command);
 
