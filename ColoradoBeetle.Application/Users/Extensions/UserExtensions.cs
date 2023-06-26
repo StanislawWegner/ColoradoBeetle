e66671af11d49ee41a5650ce67e53d8a24bd0143
Stanislaw Wegner <s.wegner@onet.eu>
2023-06-26 22:12:30 +0200

    client data update, logic and view

--- ColoradoBeetle.Application/Users/Extensions/UserExtensions.cs
+++ ColoradoBeetle.Application/Users/Extensions/UserExtensions.cs
@@ -1,4 +1,5 @@
-﻿using ColoradoBeetle.Application.Clients.Queries.GetClient;
+﻿using ColoradoBeetle.Application.Clients.Commands.EditClient;
+using ColoradoBeetle.Application.Clients.Queries.GetClient;
 using ColoradoBeetle.Domain.Entities;
 
 namespace ColoradoBeetle.Application.Users.Extensions; 
@@ -9,6 +10,24 @@ public static class UserExtensions {
             return null;
 
         return new ClientDto {
+            Id = user.Id,
+            City = user.Address?.City,
+            Country = user.Address?.Country,
+            Street = user.Address?.Street,
+            StreetNumber = user.Address?.StreetNumber,
+            ZipCode = user.Address?.ZipCode,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+        };
+    }
+
+    public static EditClientCommand ToEditClientCommand(this ApplicationUser user) {
+
+        if (user == null)
+            return null;
+
+        return new EditClientCommand {
             Id = user.Id,
             City = user.Address?.City,
             Country = user.Address?.Country,
--- ColoradoBeetle.UI/Controllers/ClientController.cs
+++ ColoradoBeetle.UI/Controllers/ClientController.cs
@@ -1,4 +1,6 @@
-﻿using ColoradoBeetle.Application.Clients.Queries.GetClient;
+﻿using Colorado.Application.Clients.Queries.GetEditClient;
+using ColoradoBeetle.Application.Clients.Commands.EditClient;
+using ColoradoBeetle.Application.Clients.Queries.GetClient;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,5 +15,24 @@ namespace ColoradoBeetle.UI.Controllers {
         public async Task<IActionResult> Client() {
             return View(await Mediator.Send(new GetClientQuery { UserId = UserId}));
         }
+
+        public async Task<IActionResult> EditClient() {
+            return View(await Mediator.Send(new GetEditClientQuery { UserId = UserId }));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditClient(EditClientCommand command) {
+            var result = await MediatorSendValidate(command);
+
+            if (!result.IsValid)
+                return View(command);
+
+            TempData["Success"] = "Dane zostały zaktualizowane";
+
+            return RedirectToAction("Client");
+        }
     }
+
+
 }
